a96f3e63988e43b3cc5d28bf9374226c7bcbb59b
OliverBR1 <Br.0liveira@outlook.com>
2025-11-15 19:55:57 -0300

    Strandalone

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [MatIconModule, MatDividerModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
